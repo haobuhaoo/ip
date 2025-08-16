6df2cfb7949a56f69c78620364b6cd71458fda35
haobuhaoo <haoyi0509@gmail.com>
2025-08-17 00:18:53 +0800

    Implement add task and list command

--- src/main/java/Lax.java
+++ src/main/java/Lax.java
@@ -1,18 +1,44 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lax {
+    public static String showList(ArrayList<String> list) {
+        if (list.isEmpty()) return "Please add task";
+
+        StringBuilder s = new StringBuilder();
+        int n = 1;
+        for (String i : list) {
+            if (n > 1) s.append("\n");
+            s.append(n).append(". ").append(i);
+            n++;
+        }
+        return s.toString();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>(100);
 
-        System.out.println("""
-                Hello! I'm Lax.
-                What can I do for you?""");
+        String command = "start";
+        while (!command.equals("bye")) {
+            switch (command) {
+                case "start":
+                    System.out.println("""
+                            Hello! I'm Lax.
+                            What can I do for you?""");
+                    break;
 
-        String command = scanner.nextLine().trim().toLowerCase();
-        if (!command.equals("bye")) {
-            System.out.println(command);
-        } else {
-            System.out.println("Bye. Hope to see you again soon!");
+                case "list":
+                    System.out.println(showList(taskList));
+                    break;
+
+                default:
+                    taskList.add(command);
+                    System.out.println("added: " + command);
+                    break;
+            }
+            command = scanner.nextLine().trim().toLowerCase();
         }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
