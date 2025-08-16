d3211b2afb390820a2cdfff0fa24a31f0b277caa
haobuhaoo <haoyi0509@gmail.com>
2025-08-16 23:37:20 +0800

    Add echo

--- src/main/java/Lax.java
+++ src/main/java/Lax.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Lax {
     public static void main(String[] args) {
-        String greet = """
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("""
                 Hello! I'm Lax.
-                What can I do for you?
-                """;
-        String exit = "Bye. Hope to see you again soon!";
-        System.out.println(greet);
-        System.out.println(exit);
+                What can I do for you?""");
+
+        String command = scanner.nextLine().trim().toLowerCase();
+        if (!command.equals("bye")) {
+            System.out.println(command);
+        } else {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
     }
 }
