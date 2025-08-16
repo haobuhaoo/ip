27db954e80fd89f9a9d4aae219856c863c2dadaf
haobuhaoo <haoyi0509@gmail.com>
2025-08-16 23:05:56 +0800

    Rename chatbot

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Lax User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Lax.java
@@ -0,0 +1,12 @@
+public class Lax {
+    public static void main(String[] args) {
+        String logo = """
+                 ____        _       \s
+                |  _ \\ _   _| | _____\s
+                | | | | | | | |/ / _ \\
+                | |_| | |_| |   <  __/
+                |____/ \\__,_|_|\\_\\___|
+                """;
+        System.out.println("Hello from\n" + logo);
+    }
+}
