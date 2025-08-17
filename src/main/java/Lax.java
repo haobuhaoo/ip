873da53a78b084815aba7322eaaabcbaec14ab1b
haobuhaoo <haoyi0509@gmail.com>
2025-08-17 23:00:31 +0800

    Add delete command

--- src/main/java/Lax.java
+++ src/main/java/Lax.java
@@ -2,6 +2,46 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lax {
+    private static String showList(ArrayList<Task> list) {
+        if (list.isEmpty()) return "There is no task in your list.";
+
+        StringBuilder s = new StringBuilder("Here are the tasks in your list:");
+        int n = 1;
+        for (Task i : list) {
+            s.append("\n").append(n).append(". ").append(i.toString());
+            n++;
+        }
+        return s.toString();
+    }
+
+    private static void labelTask(String[] cmd, ArrayList<Task> taskList, boolean mark) throws InvalidCommandException {
+        if (taskList.isEmpty()) throw new InvalidCommandException("No task to be " + (mark ? "marked" : "unmarked"));
+        if (cmd.length != 2) throw new InvalidCommandException("eg. mark 1\neg. unmark 1");
+
+        try {
+            Task t = taskList.get(Integer.parseInt(cmd[1]) - 1);
+            if (mark) {
+                if (t.isCompleted()) {
+                    throw new InvalidCommandException("Task \"" + t.getName() + "\" is already marked as done");
+                }
+
+                t.markTask();
+                System.out.println("Nice! I've marked this task as done:\n  " + t);
+            } else {
+                if (!t.isCompleted()) {
+                    throw new InvalidCommandException("Task \"" + t.getName() + "\" is already marked as not done");
+                }
+
+                t.unmarkTask();
+                System.out.println("OK, I've marked this task as not done yet:\n  " + t);
+            }
+        } catch (NumberFormatException e) {
+            throw new InvalidCommandException("eg. mark 1\neg. unmark 1");
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidCommandException("Invalid task number");
+        }
+    }
+
     private static void addTask(String command, ArrayList<Task> taskList, String type) throws InvalidCommandException {
         Task t;
         switch (type) {
@@ -41,57 +81,30 @@ public class Lax {
                 + "\nNow you have " + taskList.size() + " tasks in the list.");
     }
 
-    private static String showList(ArrayList<Task> list) {
-        if (list.isEmpty()) return "There is no task in your list.";
+    private static void deleteTask(String[] cmd, ArrayList<Task> taskList) throws InvalidCommandException {
+        if (taskList.isEmpty()) throw new InvalidCommandException("No task to delete.");
+        if (cmd.length != 2) throw new InvalidCommandException("eg. delete 1");
 
-        StringBuilder s = new StringBuilder("Here are the tasks in your list:");
-        int n = 1;
-        for (Task i : list) {
-            s.append("\n").append(n).append(". ").append(i.toString());
-            n++;
+        try {
+            Task t = taskList.remove(Integer.parseInt(cmd[1]) - 1);
+            System.out.println("Noted. I've removed this task:\n  " + t
+                    + "\nNow you have " + taskList.size() + " tasks in the list.");
+        } catch (NumberFormatException e) {
+            throw new InvalidCommandException("eg. delete 1");
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidCommandException("Invalid task number");
         }
-        return s.toString();
     }
 
     private static void cmdFunction(String command, ArrayList<Task> taskList) throws InvalidCommandException {
         String[] cmd = command.split(" ");
-        boolean emptyList = taskList.isEmpty();
 
         switch (cmd[0]) {
             case "start" -> System.out.println("Hello! I'm Lax.\nWhat can I do for you?");
             case "list" -> System.out.println(showList(taskList));
-            case "mark", "unmark" -> {
-                boolean mark = cmd[0].equals("mark");
-                if (emptyList) {
-                    throw new InvalidCommandException("No task to be " + (mark ? "marked" : "unmarked"));
-                } else if (cmd.length != 2) {
-                    throw new InvalidCommandException("eg. mark 1\neg. unmark 1");
-                }
-
-                try {
-                    Task t = taskList.get(Integer.parseInt(cmd[1]) - 1);
-                    if (mark) {
-                        if (t.isCompleted()) {
-                            throw new InvalidCommandException("Task \"" + t.getName() + "\" is already marked as done");
-                        }
-
-                        t.markTask();
-                        System.out.println("Nice! I've marked this task as done:\n  " + t);
-                    } else {
-                        if (!t.isCompleted()) {
-                            throw new InvalidCommandException("Task \"" + t.getName() + "\" is already marked as not done");
-                        }
-
-                        t.unmarkTask();
-                        System.out.println("OK, I've marked this task as not done yet:\n  " + t);
-                    }
-                } catch (NumberFormatException e) {
-                    throw new InvalidCommandException("eg. mark 1\neg. unmark 1");
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidCommandException("Invalid task number");
-                }
-            }
+            case "mark", "unmark" -> labelTask(cmd, taskList, cmd[0].equals("mark"));
             case "todo", "deadline", "event" -> addTask(command, taskList, cmd[0]);
+            case "delete" -> deleteTask(cmd, taskList);
             default -> throw new InvalidCommandException("");
         }
     }
