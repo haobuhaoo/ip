4dadf59d43f81d7abd22f2de32555c7d1f6963c1
haobuhaoo <haoyi0509@gmail.com>
2025-08-17 23:21:41 +0800

    Add the use of enum in code

--- src/main/java/Lax.java
+++ src/main/java/Lax.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lax {
+    public enum CommandList {START, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE}
+
+    public enum TaskType {TODO, DEADLINE, EVENT}
+
     private static String showList(ArrayList<Task> list) {
         if (list.isEmpty()) return "There is no task in your list.";
 
@@ -44,38 +48,43 @@ public class Lax {
 
     private static void addTask(String command, ArrayList<Task> taskList, String type) throws InvalidCommandException {
         Task t;
-        switch (type) {
-            case "todo" -> {
-                try {
-                    t = new Todo(command.substring(5));
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidCommandException("eg. todo borrow book");
+        try {
+            switch (TaskType.valueOf(type.toUpperCase())) {
+                case TODO -> {
+                    try {
+                        t = new Todo(command.substring(5));
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new InvalidCommandException("eg. todo borrow book");
+                    }
                 }
-            }
-            case "deadline" -> {
-                try {
-                    String temp = command.substring(9);
-                    String[] split = temp.split("/by");
-                    t = new Deadline(split[0].trim(), split[1].trim());
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidCommandException("eg. deadline return book /by Sunday");
+                case DEADLINE -> {
+                    try {
+                        String temp = command.substring(9);
+                        String[] split = temp.split("/by");
+                        t = new Deadline(split[0].trim(), split[1].trim());
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new InvalidCommandException("eg. deadline return book /by Sunday");
+                    }
                 }
-            }
-            case "event" -> {
-                try {
-                    String temp = command.substring(6);
-                    String[] split = temp.split("/from");
-                    String[] timing = split[1].split("/to");
-                    t = new Event(split[0].trim(), timing[0].trim(), timing[1].trim());
-                } catch (IndexOutOfBoundsException e) {
-                    throw new InvalidCommandException("eg. event project meeting /from Mon 2pm /to 4pm");
+                case EVENT -> {
+                    try {
+                        String temp = command.substring(6);
+                        String[] split = temp.split("/from");
+                        String[] timing = split[1].split("/to");
+                        t = new Event(split[0].trim(), timing[0].trim(), timing[1].trim());
+                    } catch (IndexOutOfBoundsException e) {
+                        throw new InvalidCommandException("eg. event project meeting /from Mon 2pm /to 4pm");
+                    }
                 }
+                default -> throw new InvalidCommandException("""
+                        eg. todo borrow book
+                        eg. deadline return book /by Sunday
+                        eg. event project meeting /from Mon 2pm /to 4pm""");
             }
-            default -> throw new InvalidCommandException("""
-                    eg. todo borrow book
-                    eg. deadline return book /by Sunday
-                    eg. event project meeting /from Mon 2pm /to 4pm""");
+        } catch (IllegalArgumentException e) {
+            throw new InvalidCommandException("");
         }
+
         taskList.add(t);
         System.out.println("Got it. I've added this task to the list:\n  " + t
                 + "\nNow you have " + taskList.size() + " tasks in the list.");
@@ -99,12 +108,12 @@ public class Lax {
     private static void cmdFunction(String command, ArrayList<Task> taskList) throws InvalidCommandException {
         String[] cmd = command.split(" ");
 
-        switch (cmd[0]) {
-            case "start" -> System.out.println("Hello! I'm Lax.\nWhat can I do for you?");
-            case "list" -> System.out.println(showList(taskList));
-            case "mark", "unmark" -> labelTask(cmd, taskList, cmd[0].equals("mark"));
-            case "todo", "deadline", "event" -> addTask(command, taskList, cmd[0]);
-            case "delete" -> deleteTask(cmd, taskList);
+        switch (CommandList.valueOf(cmd[0].toUpperCase())) {
+            case START -> System.out.println("Hello! I'm Lax.\nWhat can I do for you?");
+            case LIST -> System.out.println(showList(taskList));
+            case MARK, UNMARK -> labelTask(cmd, taskList, cmd[0].equals("mark"));
+            case TODO, DEADLINE, EVENT -> addTask(command, taskList, cmd[0]);
+            case DELETE -> deleteTask(cmd, taskList);
             default -> throw new InvalidCommandException("");
         }
     }
